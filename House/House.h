fb389f7e53492772cc0d4cf6405ae6dbf7e79406
Марина <marikaa@mail.ru>
2018-04-23 07:06:09 +0600

    -

--- House/House.h
+++ House/House.h
@@ -1,52 +1,68 @@
 #pragma once
 #include<string>
 using namespace std;
+
+/*5.House: Адрес, Этаж, Количество комнат, Площадь. 
+Создать массив объектов. Вывести: 
+a) список квартир, имеющих заданное число комнат; 
+b) список квартир, имеющих заданное число комнат, и расположенных на этаже, который находится в заданном промежутке; 
+c) список квартир, имеющих площадь, превосходящую заданную. 
+*/
 class House {
 private:
 	int reg_no;
-	class address {
-	private:
-		string street;
-		int flat_number;
-	public:
-		address() {
-			street = "";
-			flat_number = 0;
-		}
-		address(string street, int flat_number) {
-			this->street = street;
-			this->flat_number = flat_number;
-		}
-		//accessors
-		string getStreet() { return street; }
-		int getFlatNumber(){return flat_number;	}
+	//class address {
+	//private:
+	//	string street;
+	//	int house_number;
+	//public:
+	//	address() {
+	//		street = "";
+	//		house_number = 0;
+	//	}
+	//	address(string street, int house_number) {
+	//		this->street = street;
+	//		this->house_number =house_number;
+	//	}
+	//	//accessors
+	//	string getStreet() { return street; }
+	//	int getHouseNumber(){return house_number;	}
 
-		//mutators
-		void setStreet(string street) {this->street == street;	}
-		void setFlatNumber(int flat_number) { this->flat_number == flat_number; }
-	}house_add;
+	//	//mutators
+	//	void setStreet(string street) {this->street = street;	}
+	//	void setHouseNumber(int house_number) { this->house_number = house_number; }
+	//}house_add;
+	string street;
+	int houseNum;
 	int floor;
 	int room_number;
 	double area;
 
 public:
-	House() :house_add() {
-		reg_no = 0; floor = 0;room_number = 0;area = 0;	}
-	House(int reg_no, string street, int floor, int room_number, double area) :house_add(street, room_number)
+	House() /*:house_add()*/ {
+		reg_no = 0; street = ""; houseNum = 0; floor = 0; room_number = 0; area = 0;	}
+	House(int reg_no, string street, int house_number, int floor, int room_number, double area)/* :house_add(street, house_number)*/
 	{
-		
+		this->street = street;
+		this->houseNum = houseNum;
 		this->reg_no = reg_no;
 		this->floor = floor;
 		this->room_number = room_number;
 		this->area = area;}
-
 public:
+	/*address *add;
+	add = new address a;*/
 	//accessors
 	int getReg_no() { return reg_no; }
 	int getFloor() { return floor; }
 	int getRoomNum() { return room_number; }
 	double getArea() { return area; }
+	string getStreet() { return street; }
+	int getHouseNum() { return houseNum; }
+
 	//mutators
+	void setHouseStr(string street) {this->street = street;}
+	void setHouseNum(int houseNum) {this->houseNum = houseNum;}
 	void setReg_no(int reg_no) {this->reg_no = reg_no;	}
 	void setFloor(int floor) { this->floor = floor; }
 	void setRoomNum(int room_number) { this->room_number = room_number; }
---
+++ House/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<string>
+#include<ctime>
+#include<fstream> // библиотека работы с файлами
+#include"House.h"
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	
+	int k;
+	cout << "Enter tasks number:";
+	cin >> k;
+
+	switch (k) {
+
+	case 1: {
+		ifstream in;
+		ofstream out;
+		out.open("write.txt");
+		in.open("House.txt");
+		string house_addStreet;
+		int floor, room_number, area,reg_no, house_addNum;
+		
+		House *h;
+		h = new House[10];
+		
+		int i = 0;
+		if (!in) {
+			cerr << "File open error" << endl;
+		}
+		else
+		{
+			while (!in.eof()) {
+				in >> reg_no>>house_addStreet>>house_addNum >>floor >> room_number >> area;
+				House H(reg_no,house_addStreet, house_addNum, floor, room_number, area);
+				h[i++] = H;
+			}}
+
+		for (int k = 0; k < 10; k++) {
+			cout << h[k].getReg_no<<h[k].getStreet<<h[k].getHouseNum<<h[k].getFloor<<h[k].getRoomNum<<h[k].getArea<< endl;
+		}
+	}break;
+	}
+}
